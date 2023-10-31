012daa61a7843c4980c34dcb949f57ec6dd423a2
Fabian Wittgen <wittgenfabian@gmail.com>
2023-10-31 16:39:41 +0100

    sgfmhuidohh

--- MonsterFight/Program.cs
+++ MonsterFight/Program.cs
@@ -10,39 +10,6 @@ for (int i = 0; i != (int) ERace.None; i++)
     Console.WriteLine((ERace)i);
 }
 
-Console.WriteLine("Monster 1: ");
-while (!int.TryParse(Console.ReadLine(), out playerChoice[0]))
-{
-    Console.WriteLine("Try again! See above!");
-}
-
-Console.WriteLine("Monster 2: ");
-while (!int.TryParse(Console.ReadLine(), out playerChoice[1]))
-{
-    Console.WriteLine("Try again! See above!");
-}
-
-if (playerChoice[0] == playerChoice[1])
-{
-    Console.WriteLine("A monster can't fight it's own race!");
-}
-
-for (int i = 0; i < 2; i++)
-{
-    switch (playerChoice[i])
-    {
-        case 1:
-            monster[i] = new Orc();
-            break;
-        case 2:
-            monster[i] = new Troll();
-            break;
-        case 3:
-            monster[i] = new Goblin();
-            break;
-    }
-}
-
 
 // while (monster[0].Hp > 0 || monster[1].Hp > 0)
 // {
